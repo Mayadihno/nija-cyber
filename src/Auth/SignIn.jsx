23fa21a296ab437838220a9c80dd87e5f5ce5dd2
Mayadihno <mayadihno@gmail.com>
2023-05-13 06:02:47 +0100

    firebase deploy

--- src/Auth/SignIn.jsx
+++ src/Auth/SignIn.jsx
@@ -92,7 +92,7 @@ export const SignIn = ({ setScores }) => {
     e.preventDefault();
     setLoginLoading(true);
     sendSignInLinkToEmail(auth, email, {
-      url: "https://nija-donation.netlify.app/protected/welcome",
+      url: "https://react-cyber.firebaseapp.com/protected/welcome",
       handleCodeInApp: true,
     })
       .then(() => {
